0b8a31d03a9dabae33f9c24fd806f9e140f584b5
the-last-plesiosaur <thelast@plesiosaur.dev>
2026-08-04 19:10:03 -0400

    Prototyping central table concept

--- src/main/java/dev/plesiosaur/Neurosync.java
+++ src/main/java/dev/plesiosaur/Neurosync.java
@@ -3,6 +3,8 @@ package dev.plesiosaur;
 
 import javax.swing.*;
 import java.awt.*;
+import java.time.ZonedDateTime;
+import java.util.UUID;
 
 public class Neurosync {
     static void main() {
@@ -15,8 +17,8 @@ public class Neurosync {
         frame.setSize(1000, 800);
 
         JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File");
-        JMenu editMenu = new JMenu("Edit");
+        JMenu fileMenu = new JMenu("Vault");
+        JMenu editMenu = new JMenu("Shard");
         JMenu importMenu = new JMenu("Import");
 
         menuBar.add(fileMenu);
@@ -25,7 +27,7 @@ public class Neurosync {
 
         JMenuItem openItem = new JMenuItem("Open");
         JMenuItem saveItem = new JMenuItem("Save");
-        JMenuItem exitItem = new JMenuItem("Exit");
+        JMenuItem exitItem = new JMenuItem("Close");
 
         exitItem.addActionListener(e -> {
             System.exit(0);
@@ -38,6 +40,10 @@ public class Neurosync {
 
         frame.setJMenuBar(menuBar);
 
+        JPanel shardPanel = shardTable();
+        frame.getContentPane().add(shardPanel);
+
+        /*
         JPanel sidePanel = sidebar();
         sidePanel.setPreferredSize(new Dimension(250, 0));
 
@@ -46,12 +52,42 @@ public class Neurosync {
 
         JPanel contentPanel = new JPanel(new BorderLayout());
         contentPanel.add(sidePanel, BorderLayout.WEST);
-        contentPanel.add(mainPanel, BorderLayout.CENTER);
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, mainPanel, detailsPanel());
+        splitPane.setBorder(BorderFactory.createEmptyBorder());
+
+        contentPanel.add(splitPane, BorderLayout.CENTER);
 
         frame.getContentPane().add(contentPanel);
+         */
 
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    private static JPanel shardTable() {
+        JPanel shards = new JPanel();
+        shards.setLayout(new BorderLayout());
+
+        String[] columnNames = new String[]{"Id", "Key", "Review", "Jacks", "Flagged"};
+        Object[][] data = {
+                {UUID.randomUUID(), "Vault A", ZonedDateTime.now(), 6, false},
+                {UUID.randomUUID(), "Vault B", ZonedDateTime.now(), 2, true},
+                {UUID.randomUUID(), "Vault C", ZonedDateTime.now(), 0, false},
+                {UUID.randomUUID(), "Vault A", ZonedDateTime.now(), 1, true}
+        };
+
+        JTable table = new JTable(data, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {  return false; }
+        };
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+
+        shards.add(scrollPane, BorderLayout.CENTER);
+
+        return shards;
     }
 
     private static JPanel sidebar() {
@@ -88,6 +124,15 @@ public class Neurosync {
         JScrollPane scrollPane = new JScrollPane(sidebar);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         panel.add(scrollPane, BorderLayout.CENTER);
+
+        return panel;
+    }
+
+    private static JPanel detailsPanel() {
+        JLabel title = new JLabel("Details");
+        //JPanel panel = new JPanel(new BorderLayout(5, 5));
+        JPanel panel = new JPanel();
+        panel.add(title);
 
         return panel;
     }
