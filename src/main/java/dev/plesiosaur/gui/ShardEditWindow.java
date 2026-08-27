3d8be8248451f276c83423b28e3c636f32db5f03
the-last-plesiosaur <thelast@plesiosaur.dev>
2026-08-27 10:30:22 -0400

    Implemented basic editor

--- src/main/java/dev/plesiosaur/gui/ShardEditWindow.java
+++ src/main/java/dev/plesiosaur/gui/ShardEditWindow.java
@@ -3,6 +3,8 @@ package dev.plesiosaur.gui;
 import dev.plesiosaur.model.Shard;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 
 public class ShardEditWindow extends JDialog {
@@ -16,10 +18,65 @@ public class ShardEditWindow extends JDialog {
 
         JTabbedPane tabbedPane = new JTabbedPane();
 
-
         JPanel challengePanel = new JPanel();
-        challengePanel.setLayout(new FlowLayout());
-        challengePanel.add(new JLabel("Challenge"));
+        challengePanel.setLayout(new BorderLayout());
+
+        JTextArea textArea = new JTextArea(10, 30);
+        textArea.setText(shard.getChallengeText());
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        challengePanel.add(scrollPane, BorderLayout.CENTER);
+
+        JButton previewButton = new JButton("Preview");
+        JButton commitButton = new JButton("Commit");
+        commitButton.setEnabled(false);
+
+        JButton revertButton = new JButton("Revert");
+        revertButton.setEnabled(false);
+
+        textArea.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) { handleTextChange(); }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) { handleTextChange(); }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) { handleTextChange(); }
+
+            private void handleTextChange() {
+                if(textArea.getText().trim().equals(shard.getChallengeText())) {
+                    commitButton.setEnabled(false);
+                    revertButton.setEnabled(false);
+                } else {
+                    commitButton.setEnabled(true);
+                    revertButton.setEnabled(true);
+                }
+            }
+        });
+
+        commitButton.addActionListener(e -> {
+            String newChallengeText = textArea.getText().trim();
+            shard.setChallengeText(newChallengeText);
+            commitButton.setEnabled(false);
+            revertButton.setEnabled(false);
+        });
+
+        revertButton.addActionListener(e -> {
+            textArea.setText(shard.getChallengeText());
+            commitButton.setEnabled(false);
+            revertButton.setEnabled(false);
+        });
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+        buttonPanel.add(previewButton);
+        buttonPanel.add(commitButton);
+        buttonPanel.add(revertButton);
+
+        challengePanel.add(buttonPanel, BorderLayout.SOUTH);
+
+
+
 
         JPanel responsePanel = new JPanel();
         responsePanel.setLayout(new FlowLayout());
